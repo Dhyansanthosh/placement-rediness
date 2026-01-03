43ecff95d04a09a957c42aa076d1bc831f16fe9d
Dhyansanthosh <dhyandhyan833@gmail.com>
2026-01-03 10:48:33 +0530

    new change

--- src/api/api.js
+++ src/api/api.js
@@ -7,20 +7,15 @@ const api = axios.create({
   },
 });
 
-// Attach JWT token automatically to every request
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
